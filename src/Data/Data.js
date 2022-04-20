7b1935cdfa7ab00d40e543ebfcd5b7b50e6c82c8
ynkwon <>
2022-04-20 17:43:57 +0900

    [1] only nav bar is updated

---
+++ src/Data/Data.js
@@ -0,0 +1,26 @@
+// Sidebar imports
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+
+export const SidebarData = [
+    {
+        icon: UilEstate,
+        heading: "대시보드",
+    },
+    {
+        icon: UilChart,
+        heading: "프로젝트 관리",
+    },
+    {
+        icon: UilPackage,
+        heading: "개인정보관리",
+    },
+];
+
